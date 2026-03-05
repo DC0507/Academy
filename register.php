41c248934ab8a5eea8e383afcad8af1b948e104b
David Calderón <dcalderon179105@gmail.com>
2026-03-04 18:36:18 -0600

    Update register.php
    Adding comments and modifications.

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ include './config/db.php';
 //Inicializa la variable message
 $message = "";
 
-//Si el método de envío del formulario es POST
+//Evalua si el método de envío del formulario es POST (para prevenir que se acceda directamente)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //Almacena los valores de los campos name, email y password en las respectivas variables
@@ -25,9 +25,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Agrega los valores de los párametros a la consulta
         $stmt->bind_param("sss", $name, $email, $hashedPassword);
 
+        //Ejecuta la consulta
         if ($stmt->execute()) {
+            //Si se ejecuta correctamente devuelve, message almacena un mensaje exitoso
             $message = "Registration successful.";
         } else {
+            //Si falla, message almacena el error
             $message = "Error: " . $stmt->error;
         }
 
@@ -36,22 +39,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+<!--Incluye el contenido de header.php-->
 <?php include './includes/header.php'; ?>
 
+<!--Código del Formulario en HTML-->
 <h2>Registro</h2>
 
 <form action="" method="post">
     <label for="name">Nombre:</label><br>
-    <input type="text" name="name" id="name">
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email">
-    <label for="password">contraseña:</label>
-    <input type="password" name="password" id="password">
+    <input type="text" name="name" id="name"><br>
+    <label for="email">Email:</label><br>
+    <input type="email" name="email" id="email"><br>
+    <label for="password">Contraseña:</label><br>
+    <input type="password" name="password" id="password"><br>
     <button type="submit">Registrar</button>
 </form>
 
 <p>
+    <!--Imprime el contenido de message-->
     <?php echo $message; ?>
 </p>
 
+<!--Incluye el contenido de footer.php-->
 <?php include './includes/footer.php'; ?>
